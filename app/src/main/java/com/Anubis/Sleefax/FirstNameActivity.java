3b10d560c38454716e10b557c978b951b037a375
Tushti Gulati <tushtigulati5@gmail.com>
2020-07-14 22:11:23 +0530

    Corrected UI

--- app/src/main/java/com/Anubis/Sleefax/FirstNameActivity.java
+++ app/src/main/java/com/Anubis/Sleefax/FirstNameActivity.java
@@ -509,6 +509,27 @@ public class FirstNameActivity extends AppCompatActivity {
             }
         });
 
+        numberTV.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                EmailText = Email.getText().toString();
+                continueButton.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                EmailText = Email.getText().toString();
+                continueButton.setVisibility(View.VISIBLE);
+            }
+        });
+
+
 
         /////////////////////////////////////////////////////new UI///////////////////////////////////////////////
 
